a71acd2b78b0f600d202969ac9ae39b945cbb29d
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-29 03:47:39 +0500

    add app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF07A997);
+  static const Color blue = Color(0xFF1787E7);
+  static const Color fieldColor = Color(0xFFDCDEE6);
+  static const Color containerColor = Color(0xFFF7F7F7);
+  static const Color greenCard = Color(0xFF18B368);
+  static const Color blueCard = Color(0xFF3F85ED);
+  static const Color orangeCard = Color(0xFFFBA363);
+  static const Color pinkColor = Color(0xFFF5789F);
+  static const Color purpleColor = Color(0xFF905FDD);
+
+  static const Color textGrey = Color(0xFF6B6F80);
+
+  static const Color white = Color(0xFFffffff);
+  static const Color black = Color(0xFF292B33);
+  static const Color red = Color(0xFFFF0000);
+  static const Color gry = Color(0xFF545766);
+  static const Color lightBlack = Color(0xff161616);
+  static const Color aqua = Color(0xff26DBDC);
+  static const Color deepSkyBlue = Color(0xff1787E7);
+
+  static const Color transparent = Colors.transparent;
+  static const Color lightGreyColors = Color(0xff838799);
+  static const Color masterGradiantOne = Color(0xffBD7B26);
+  static const Color masterGradiantTwo = Color(0xffD19A3F);
+  static const Color backgroundColor = Color(0xffFDF9F2);
+  static const Color lightMasterThree = Color(0xffF2D8A8);
+  static const Color lightMasterTwo = Color(0xffD19A3F);
+  static const Color lightMasterOne = Color(0xffBD7B26);
+  static const Color masterColor = Color(0xff9F641D);
+  static const Color greenGrey = Color(0xff9EAEA6);
+  static const Color greenGradiantOne = Color(0xff4F5E27);
+  static const Color greenGradiantTwo = Color(0xff6A7B39);
+  static const Color stockColor = Color(0xffD0D0D0);
+  static const Color greyColor = Color(0xFFF6F7F9);
+  static const Color lightBlack1 = Color(0xff2B2A29);
+  static const Color placeHolderColor = Color(0xff708379);
+  static const Color indicatorColor = Color(0xffFDF9F2);
+  static const Color indicatorForegroundColor = masterGradiantOne;
+}
