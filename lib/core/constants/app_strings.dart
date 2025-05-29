e98da8ed89cb0c5eca3ce95f32bb28ecc140d3db
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-30 02:40:16 +0500

    add app string

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,195 @@
+class AppStrings {
+  static const String about = "About";
+  static const String paymentMethods = "Payment Methods";
+  static const String accountInformation = "Account Information";
+  static const String add = "Add";
+  static const String addUser = "Add user";
+  static const String appName = "SparkD";
+  static const String alreadyHaveAccount = "Are you already a member? ";
+
+  static const String back = "Back";
+  static const String verify = "Verify";
+  static const String verifyYourMail = "Verify your mail";
+  static const String verifyYourMailDescription = "Please enter the 4 digit code sent to ";
+  static const String send = "Send";
+  static const String verification = "Verification";
+  static const String verificationDescription = "Please enter your email address to receive a verification code";
+  static const String settings = "Settings";
+  static const String profile = "Profile";
+  static const String cannotBeEmpty = "cannot be empty";
+  static const String clear = "Clear";
+  static const String yes = "Yes";
+  static const String cancel = "Cancel";
+  static const String continueText = "Continue";
+  static const String createAccount = "Create Account";
+  static const String createAccountDescription = "To save your information\nyou need to create an account";
+  static const String date = "Date";
+  static const String infiniteSparkd = "Infinite SparkdAI";
+  static const String delete = "Delete";
+  static const String deleteChat = "Delete Chat";
+  static const String deleteChatDescription = "Are you sure you want to delete this chat?";
+  static const String giveMeLine = "Give me a more";
+  static const String dontHaveAccount = "Don’t have an account?";
+
+  static const String email = "Email";
+  static const String tapToStop = "Tap to stop recording";
+  static const String everythingSetup = "Everything is set up,\nLet’s start a sparkd!";
+  static const String forgotPassword = "Forgot Password";
+  static const String fullName = "Full Name";
+
+  static const String galleryPhoto = "Gallery Photo";
+  static const String getStarted = "Get Started";
+  static const String login = "Log in";
+  static const String clearAll = "Clear all conversations";
+  static const String clearAllDescription = "Are you sure you want to delete all you conversations?";
+  static const String createPin = "Create a PIN";
+  static const String newPin = "New PIN";
+  static const String oldPin = "Old PIN";
+  static const String changePin = "Change PIN";
+  static const String enterYourNewPin = "Enter your new pin";
+  static const String logout = "Log out";
+  static const String logoutDescription = "Are you sure do you want to log out?";
+  static const String deletetDescription = "Are you sure you want to permanently delete your account? This action cannot be undone, and all your data will be erased. Please note, any active subscriptions will remain in effect. Make sure to cancel them in your account settings to avoid future charges.";
+  static const String deletetaccont = "Are you sure you want to delete your account?";
+
+  static const String usePin = "Use PIN";
+  static const String touchTheFingerPrint = "Touch the fingerprint sensor";
+  static const String loginToSparkd = "Login to Sparkd";
+  static const String confirmYourIdentity = "Confirm your identity";
+
+  static const String notMember = "Not a member yet?";
+
+  static const String loginDescription = "Fill in details to login";
+  static const String letsGo = "Let's go";
+
+  static const String password = "Password";
+  static const String goBackToProfile = "Go back to Profile";
+  static const String pinCreated = "PIN created";
+  static const String pinCreatedSuccess = "PIN created successfully";
+  static const String paymentConfirmation = "We received\nyour payment!";
+  static const String paymentConfirmationDescription = "Let’s start your sparkd!";
+  static const String confirmPassword = "Confirm password";
+  static const String selectDate = "Select Date";
+  static const String knowThePerson = "Let me get to know the person";
+  static const String knowThePersonDescription = "To start your sparkd\nwe need more information";
+  static const String signUp = "Sign up";
+  static const String signUpDescription = "Fill in details to sign up";
+  static const String submit = "Submit";
+  static const String facebook = "Facebook";
+  static const String chats = "Chats";
+  static const String appUpdates = "App Updates";
+  static const String appUpdatesDescription = "When we have new updates";
+  static const String chat = "Chat";
+  static const String chatUpdates = "Chat Updates";
+  static const String chatUpdatesDescription = "Notifications when you get new suggestions";
+  static const String chatDescription = "Notices related to your chats";
+  static const String archived = "Archived";
+  static const String notifications = "Notifications";
+  static const String needHelp = "Need Help?";
+
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+
+  static const String termsConditions = "Terms of Services";
+  static const String appSubscription = "Subscription ";
+
+  static const String enterEmail = 'Enter your email';
+  static const String enterName = 'Enter your name';
+  static const String enterPassword = 'Enter your password';
+  static const String yourGenderHeading = 'What is your gender?';
+  static const String yourAgeHeading = 'How old are you?';
+  static const String theirAgeHeading = "What's their age?";
+  static const String theirGenderHeading = "What's their gender?";
+  static const String theirPersonalityHeading = 'What’s their personality?';
+  static const String yourObjectiveHeading = 'What’s your objective?';
+  static const String welcome = 'Welcome';
+  static const String pay = 'Pay';
+  static const String subscriptionWillRenewAnd = 'subscriptions will renew unless cancelled\nin your Google Play Settings';
+    static const String subscriptionWillRenewIOS = 'subscriptions will renew unless cancelled\nin your App Store Settings';
+  static const String freeTrial = 'Enjoy a 7-day Free Trial, then \njust \$19.99/month afterward!';
+  static const String unlockFreeTrial = 'Unlock Free Trial';
+  static const String flameOn = 'Flame On, Game Strong';
+  static const String unlimitedSparks = 'Unlimited\nsparks';
+  static const String personalCoach = 'Personal\ncoach';
+  static const String provenDates = 'Proven to\nget dates';
+  static const String welcomeBack = 'Welcome back!';
+  static const String orSignupWith = 'Or sign up with';
+  static const String uploadScreenShot = 'Upload your conversation';
+  static const String yourPersonalityHeading = 'What is your\npersonality type?';
+
+  static const String or = 'or';
+  static const String startASparkd = 'Start a sparkd';
+  static const String getSparkdLines = 'Pickup Line';
+  static const String sparkdLines = 'Sparkd lines';
+  static const String letstartASparkd = "Lets Start!";
+
+  static const String enterManually = "Manual Text";
+
+  static const String next = "Next";
+  static const String createNewPassword = "Create a new Password";
+  static const String createNewPin = "Create a new PIN";
+  static const String backToHome = "Back to home";
+  static const String bioAuth = "Biometric Authentication";
+  static const String bioAuthDescription = "Login to your sparkds using your phone’s biometric credentials";
+  static const String bioAuthSetup = "Biometric Authentication\nis now set up";
+  static const String bioAuthSetupDescription = "Turn off anytime from settings";
+  static const String enableBio = "Enable Biometrics";
+  static const String enableBioLogin = "Enable biometric login";
+  static const String useBiometricVerification = "Use Biometric";
+  static const String getMySparkd = "Create Chat";
+  static const String name = "Name";
+  static const String enterYourText = "Enter your text";
+  static const String enterYourPin = "Enter your pin";
+  static const String enterPin = "Enter PIN";
+  static const String enterNewPassword = "Enter New Password";
+  static const String enterNewPin = "Enter New PIN";
+  static const String confirmPin = "Confirm PIN";
+  static const String forgotYourPin = "Forgot your pin";
+  static const String forgotPin = "Forgot pin";
+  static const String allSetDescription = "Save this person’s name to keep\ntrack of all your conversations";
+  static const String allSet = "You’re all set!";
+  static const String listening = "Listening";
+
+  ///Common String
+  static const String errorFirstName = "Please enter first name";
+  static const String errorLastName = "Please enter last name";
+  static const String errorDateBirth = "Please enter date of birth";
+  static const String errorEmail = "Please enter email";
+  static const String errorUsername = "Please enter username";
+  static const String errorOTP = "Please enter OTP";
+  static const String errorValidOTP = "Please enter valid OTP";
+  static const String errorValidEmail = "Please enter valid email";
+  static const String errorMobileNo = "Please enter mobile number";
+  static const String errorMobileNoVerify = "Please verify your phone number";
+  static const String selectedCountryCode = "Please select country code";
+  static const String selectedObjective = "Please select your objective";
+  static const String selectedGender = "Please select gender";
+  static const String selectedAge = "Please select age";
+  static const String selectedPersonality = "Please select personality";
+  static const String errorName = "Please enter name";
+
+  static const String errorStreetName = "Please enter street name";
+  static const String errorApartmentName = "Please enter apt.,suit,unit";
+  static const String errorCity = "Please enter city";
+  static const String errorState = "Please enter state";
+  static const String errorZip = "Please enter zip code/postal code";
+  static const String errorSearch = "Please enter address";
+  static const String errorEmptyPasscode = "Please enter the passcode sent to the email";
+  static const String errorMessageSubject = "Please enter subject";
+  static const String errorMessageMessage = "Please enter message";
+
+  static const String errorEmptyPassword = "Please enter password";
+  static const String errorEmptyConfirmPassword = "Please enter confirm password";
+  static const String errorEmptyPin = "Please enter pin";
+  static const String errorEmptyNewPin = "Please enter new pin";
+  static const String errorEmptyConfirmPin = "Please enter confirm pin";
+  static const String errorValidPassword = "Password must be more than 8 characters long including lower case, upper case, number and a special character.";
+  static const String errorNotMatchPassword = "Password and re-enter password does not match";
+  static const String errorNotMatchPin = "Pin and re-enter pin does not match";
+  static const String errorPleaseEnterIncome = "Please enter income";
+  static const String errorPleaseEnterEmailAndPassword = "Please enter email and password";
+  static const String errorPleaseSelectRelation = "Please select relationship";
+  static const String pleaseSelectTermsAndCondition = "Please select terms and condition";
+  static const String pleaseEnterAccountNumber = "Please enter account number";
+  static const String pleaseEnterValidAccountNumber = "Please enter valid account number";
+}
