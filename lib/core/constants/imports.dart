77e881763557c4a9bdd54bb2a72ce754e9c41cd6
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-30 02:40:54 +0500

    export imports

---
+++ lib/core/constants/imports.dart
@@ -0,0 +1,30 @@
+export 'package:flutter/material.dart';
+
+export 'package:veritey/core/constants/app_strings.dart';
+export 'package:veritey/core/components/sb.dart';
+export 'package:veritey/gen/assets.gen.dart';
+
+export 'package:veritey/core/components/app_button.dart';
+export 'package:veritey/core/components/app_bar.dart';
+
+///Enums
+export 'package:veritey/core/enums/genders.dart';
+export 'package:veritey/core/enums/personality.dart';
+
+export 'package:veritey/core/enums/chat_type.dart';
+export 'package:veritey/models/message_model.dart';
+
+/// Pages=
+
+/// Widgets=
+
+/// Extensions
+export 'package:veritey/core/extensions/build_context_extension.dart';
+
+/// PACKAGE
+export 'package:get/get.dart'
+    hide ContextExtensionss, FormData, MultipartFile, Response;
+export 'package:flutter_svg/flutter_svg.dart';
+
+export 'package:veritey/core/constants/constants.dart';
+export 'package:veritey/pages/dashboard/dashboard.dart';
