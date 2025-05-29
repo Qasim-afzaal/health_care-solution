d220222d5eb799ff3e756ad2c57df543d96d31f9
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-30 02:47:37 +0500

    add assets utils

---
+++ lib/core/Utils/assets_util.dart
@@ -0,0 +1,12 @@
+class CustomImages {
+  static const carPic = 'assets/images/car_pic.png';
+  static const sPic = 'assets/images/s.png';
+  static const sPicBlack = 'assets/images/s_black.png';
+  static const wheelIcon = 'assets/images/wheel_icon.png';
+  static const googleIcon = 'assets/images/google_logo.png';
+  static const notificationBellIcon = 'assets/images/notification_bell.gif';
+  static const successGif = 'assets/images/success.gif';
+  static const placeholderAvatar = 'assets/images/placeholder_avatar.png';
+  static const ownerHomeCar = 'assets/images/owner_home_car.png';
+  static const carBackground = 'assets/images/car_background.png';
+}
