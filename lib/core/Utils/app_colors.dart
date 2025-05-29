a23d10b7d5fc1722150d07c13b6d6e9aa35317b4
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-05-30 02:47:31 +0500

    add app colors

---
+++ lib/core/Utils/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const secondaryColor = Color(0xffCDFF49);
+
+  // static  Color secondaryColor = Colors.yellow.shade300;
+  static const blackColor = Color(0xff000000);
+  static const whiteColor = Color(0xffFFFFFF);
+  static const fieldBackgroundColor = Color(0xffF8F8F7);
+  static const textHintColor = Color(0xff777777);
+  static const textFieldBorderColor = Color(0xffEEEBEB);
+  static const skinToneColor = Color(0xffF1EEE9);
+  static const successColor = Color(0xff22FBB6);
+  static const errorColor = Color(0xffE33629);
+  static const turquoiseColor = Color(0xffE6DFF1);
+  static const grayColor = Color(0xff484848);
+}
