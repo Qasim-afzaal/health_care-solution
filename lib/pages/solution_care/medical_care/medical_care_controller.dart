bf8ace1f2cfc0f2b98d0fd7c022194f6b3f3fe67
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-06-15 08:09:30 +0500

    add medical controller

---
+++ lib/pages/solution_care/medical_care/medical_care_controller.dart
@@ -0,0 +1,13 @@
+import 'package:veritey/api_repository/api_class.dart';
+import 'package:veritey/core/constants/imports.dart';
+
+class MedicalCareController extends GetxController {
+  String? id;
+  @override
+  void onInit() {
+    super.onInit();
+    if (Get.arguments != null) {
+      id = Get.arguments[HttpUtil.id] ?? "";
+    }
+  }
+}
