3121c94354f2ed1acd275692e099ea59de125774
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-06-20 03:59:57 +0500

    create app routes

---
+++ lib/routes/app_routes.dart
@@ -0,0 +1,125 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const SPLASH = _Paths.SPLASH;
+  static const ON_BOARDING = _Paths.ON_BOARDING;
+  static const PROFILE_PAGE_2 = _Paths.PROFILEPAGE_2;
+  static const ON_BOARDING_FOR_SOCIAL = _Paths.ON_BOARDING_FOR_SOCIAL;
+  static const CREATE_ACCOUNT = _Paths.CREATE_ACCOUNT;
+  static const LOGIN = _Paths.LOGIN;
+  static const SIGN_UP = _Paths.SIGN_UP;
+  static const CODE_VERIFICATION = _Paths.CODE_VERIFICATION;
+  static const CREATE_PASSWORD = _Paths.CREATE_PASSWORD;
+  static const EMAIL_VERIFICATION = _Paths.EMAIL_VERIFICATION;
+  static const PIN_VERIFICATION = _Paths.PIN_VERIFICATION;
+  static const DASHBOARD = _Paths.DASHBOARD;
+  static const OTP_SIGNIN = _Paths.OTP;
+  static const PHONE = _Paths.PHONE;
+  static const HOME = _Paths.HOME;
+  static const SHEDULE = _Paths.SHEDULE;
+  static const CARE = _Paths.CARE;
+
+  static const INBOX = _Paths.INBOX;
+  static const CHAT = _Paths.CHAT;
+  static const GATHER_NEW_CHAT_INFO = _Paths.GATHER_NEW_CHAT_INFO;
+  static const INFO = _Paths.INFO;
+  static const MANUAL_CHAT = _Paths.MANUAL_CHAT;
+  static const VOICE_CHAT = _Paths.VOICE_CHAT;
+  static const PAYMENT_CONFIRMATION = _Paths.PAYMENT_CONFIRMATION;
+  static const PAYMENT_METHOD = _Paths.PAYMENT_METHOD;
+  static const PAYMENT_PLAN = _Paths.PAYMENT_PLAN;
+  static const CREATE_PIN = _Paths.CREATE_PIN;
+  static const PIN_SUCCESS = _Paths.PIN_SUCCESS;
+  static const IMAGE_CHAT = _Paths.IMAGE_CHAT;
+  static const ALL_CHAT = _Paths.ALL_CHAT;
+  static const PROFILE = _Paths.PROFILE;
+  static const MAINPAGE = _Paths.MAINPAGE;
+  static const PICKUP_LINES = _Paths.PICKUP_LINES;
+  static const SOCIAL = _Paths.SOCIAL;
+  static const SETTINGS = _Paths.SETTINGS;
+  static const NOTIFICATION_SETTINGS = _Paths.NOTIFICATION_SETTINGS;
+  static const SPARKD_LINES_RESPONSE = _Paths.SPARKD_LINES_RESPONSE;
+  static const SPARKD_LINES = _Paths.SPARKD_LINES;
+  static const START_SPARKD = _Paths.START_SPARKD;
+  static const NEW = _Paths.NEW;
+  static const NEW2 = _Paths.NEW2;
+  static const TERMS_CONDITION = _Paths.TERMS_CONDITION;
+  static const CHAT_PAGE = _Paths.CHAT_PAGE;
+  static const LOCATION_SCREEN = _Paths.LOCATION_SCREEN;
+  static const LOCATION_SCREEN2 = _Paths.LOCATION_SCREEN2;
+  static const LOCATION_PERMISSION_SCREEN = _Paths.LOCATION_PERMISSION_SCREEN;
+  static const SELECTED_COUNTRY = _Paths.SELECTED_COUNTRY;
+  static const SELECTED_ADDRESS = _Paths.SELECTED_ADDRESS;
+  static const MENTAL_HEALTH = _Paths.MENTAL_HEALTH;
+  static const HOME_HEALTH = _Paths.HOME_HEALTH;
+  static const MEDICAL_CARE = _Paths.MEDICAL_CARE;
+  static const CARE_GIVER = _Paths.CARE_GIVER;
+  static const PROFILE_DETAIL = _Paths.PROFILE_DETAIL;
+  static const ON_DEMAND = _Paths.ON_DEMAND;
+
+  static const BOOKING_PAGE = _Paths.BOOKING_PAGE;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const SPLASH = '/splash';
+  static const NEW = '/new';
+  static const NEW2 = '/new2';
+  static const ON_BOARDING = '/on-boarding';
+  static const PROFILEPAGE_2 = '/profile_page_2';
+  static const ON_BOARDING_FOR_SOCIAL = '/on-boarding-social';
+  static const CREATE_ACCOUNT = '/create-account';
+  static const LOGIN = '/login';
+  static const SIGN_UP = '/sign-up';
+  static const CODE_VERIFICATION = '/code-verification';
+  static const CREATE_PASSWORD = '/create-password';
+  static const EMAIL_VERIFICATION = '/email-verification';
+  static const PIN_VERIFICATION = '/pin-verification';
+  static const DASHBOARD = '/dashBoard';
+  static const OTP = '/otp_Screen';
+  static const HOME = '/home';
+  static const INBOX = '/inbox';
+  static const CHAT = '/chat';
+  static const GATHER_NEW_CHAT_INFO = '/gather-new-chat-info';
+  static const INFO = '/info';
+  static const VOICE_CHAT = '/voice-chat';
+  static const PAYMENT_CONFIRMATION = '/payment-confirmation';
+  static const PAYMENT_METHOD = '/payment-method';
+  static const PAYMENT_PLAN = '/payment-plan';
+  static const IMAGE_CHAT = '/image_chat';
+  static const CREATE_PIN = '/create-pin';
+  static const PIN_SUCCESS = '/pin-success';
+  static const MAINPAGE = '/main-page';
+  static const PROFILE = '/profile';
+  static const SETTINGS = '/settings';
+  static const PICKUP_LINES = '/pickup_lines';
+  static const MANUAL_CHAT = '/manual_chat';
+  static const NOTIFICATION_SETTINGS = '/notification-settings';
+  static const SPARKD_LINES_RESPONSE = '/sparkd-lines-response';
+  static const SPARKD_LINES = '/sparkd-lines';
+  static const START_SPARKD = '/start-sparkd';
+  static const PROFILE_DETAIL = '/profile-detail';
+  static const SOCIAL = '/social';
+  static const TERMS_CONDITION = '/terms-condition';
+
+  static const LOCATION_SCREEN = '/location-screen';
+  static const LOCATION_SCREEN2 = '/location-screen2';
+  static const SELECTED_COUNTRY = '/country';
+  static const SELECTED_ADDRESS = '/address';
+  static const SHEDULE = '/shed';
+  static const ALL_CHAT = '/all_chat';
+  static const CARE = '/care';
+  static const CHAT_PAGE = '/chat_page';
+  static const MENTAL_HEALTH = '/mental_health';
+  static const HOME_HEALTH = '/home_health';
+  static const MEDICAL_CARE = '/medical_Care';
+  static const CARE_GIVER = '/care_giver';
+  static const ON_DEMAND = '/on_demand';
+  static const PHONE = '/phone';
+
+  static const BOOKING_PAGE = '/booking_page';
+
+  static const LOCATION_PERMISSION_SCREEN = '/location-permssion';
+}
