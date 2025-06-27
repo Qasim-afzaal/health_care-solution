36b12b6af41b75ed36f035e522c984e55435064b
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-06-28 03:51:03 +0500

    add profile controller

--- lib/pages/profile/profile_controller.dart
+++ lib/pages/profile/profile_controller.dart
@@ -1,9 +1,13 @@
+import 'package:dio/dio.dart' as dio;
+
 import 'package:veritey/api_repository/api_class.dart';
+import 'package:veritey/api_repository/api_function.dart';
 import 'package:veritey/core/constants/imports.dart';
 import 'package:veritey/pages/auth/login/login_response.dart';
 
 class ProfileController extends GetxController {
   LoginResponse? mainModel;
+  String? imagePath;
 
   @override
   void onInit() {
@@ -11,5 +15,32 @@ class ProfileController extends GetxController {
       mainModel = Get.arguments[HttpUtil.loginModel] ?? "";
     }
     super.onInit();
+  }
+
+  Future<void> uploadImage() async {
+    try {
+      if (imagePath == null) return;
+
+      dio.FormData formData = dio.FormData.fromMap({
+        HttpUtil.profileImageUrl: await dio.MultipartFile.fromFile(
+          imagePath!,
+          filename: imagePath!.split("/").last,
+        ),
+      });
+
+      final data = await APIFunction().patchApiCall(
+        apiName: "/users/profile-image",
+        data: formData,
+      );
+      if (data["success"] == true) {
+        mainModel = LoginResponse.fromJson(data);
+        if (mainModel != null) {
+          getStorageData.saveLoginData(mainModel!);
+          update();
+        }
+      }
+    } catch (e) {
+      utils.showToast(message: "Image upload failed: $e");
+    }
   }
 }
