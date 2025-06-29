2c98ebd8f6c12a41d7c67b759d5e06215f3e5f81
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-06-29 07:39:41 +0500

    add profile screen UI

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:veritey/core/components/logout_dialog.dart';
 import 'package:veritey/core/constants/app_colors.dart';
 import 'package:veritey/core/constants/imports.dart';
 import 'package:veritey/pages/home/home_controller.dart';
+import 'package:veritey/pages/profile/component/logout_android_dialog.dart';
 import 'package:veritey/pages/profile/profile_controller.dart';
 import 'package:veritey/routes/app_pages.dart';
 
@@ -38,69 +42,108 @@ class ProfilePage extends StatelessWidget {
                     const SizedBox(
                       height: 20,
                     ),
-                    Container(
-                      width: double.infinity,
-                      height: 70,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFFE6F0F2),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  radius: 32.5,
-                                  backgroundColor: AppColors.fieldColor,
-                                  child: Icon(
-                                    Icons.person,
-                                    color: AppColors.primary,
-                                    size: 30,
+                    GestureDetector(
+                      onTap: () {
+                        Get.toNamed(Routes.PROFILE_DETAIL);
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 70,
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFE6F0F2),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  CircleAvatar(
+                                    radius: 22,
+                                    backgroundColor: AppColors.fieldColor,
+                                    child: controller.mainModel?.data
+                                                    ?.profileImage !=
+                                                null &&
+                                            controller.mainModel!.data!
+                                                .profileImage!.isNotEmpty
+                                        ? ClipOval(
+                                            child: Stack(
+                                              alignment: Alignment.center,
+                                              children: [
+                                                Image.network(
+                                                  controller.mainModel!.data!
+                                                      .profileImage!,
+                                                  width: 44, // 2 * radius
+                                                  height: 44,
+                                                  fit: BoxFit.cover,
+                                                  loadingBuilder: (context,
+                                                      child, loadingProgress) {
+                                                    if (loadingProgress == null)
+                                                      return child;
+                                                    return const SizedBox(
+                                                      width: 16,
+                                                      height: 16,
+                                                      child:
+                                                          CircularProgressIndicator(
+                                                        strokeWidth: 2,
+                                                        color:
+                                                            AppColors.primary,
+                                                      ),
+                                                    );
+                                                  },
+                                                  errorBuilder: (context, error,
+                                                          stackTrace) =>
+                                                      const Icon(
+                                                    Icons.person,
+                                                    color: AppColors.primary,
+                                                    size: 30,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          )
+                                        : const Icon(Icons.person,
+                                            color: AppColors.primary, size: 30),
                                   ),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const SizedBox(
-                                      height: 7,
-                                    ),
-                                    Text(
-                                      controller.mainModel?.data?.username ??
-                                          "",
-                                      style: GoogleFonts.plusJakartaSans(
-                                        fontWeight: FontWeight.w600,
-                                        fontSize: 16,
-                                        color: AppColors.black,
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const SizedBox(
+                                        height: 7,
                                       ),
-                                    ),
-                                    Text(
-                                      'Company name',
-                                      style: GoogleFonts.plusJakartaSans(
-                                        fontWeight: FontWeight.w400,
-                                        fontSize: 12,
-                                        color: AppColors.black,
+                                      Text(
+                                        controller.mainModel?.data?.username ??
+                                            "",
+                                        style: GoogleFonts.plusJakartaSans(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 16,
+                                          color: AppColors.black,
+                                        ),
                                       ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            GestureDetector(
-                              onTap: () {
-                                Get.toNamed(Routes.PROFILE_DETAIL);
-                              },
-                              child: const Icon(
+                                      Text(
+                                        'Company name',
+                                        style: GoogleFonts.plusJakartaSans(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 12,
+                                          color: AppColors.black,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              const Icon(
                                 CupertinoIcons.forward,
                                 size: 19,
-                              ),
-                            )
-                          ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -177,13 +220,13 @@ class ProfilePage extends StatelessWidget {
                                   ),
                                 ),
                                 Text(
-                                  (controller.mainModel?.data?.location?.address
-                                                  ?.length ??
+                                  (controller.mainModel?.data?.location
+                                                  ?.locationAddress?.length ??
                                               0) >
                                           30
-                                      ? '${controller.mainModel?.data?.location?.address?.substring(0, 30)}...'
+                                      ? '${controller.mainModel?.data?.location?.locationAddress?.substring(0, 30)}...'
                                       : controller.mainModel?.data?.location
-                                              ?.address ??
+                                              ?.locationAddress ??
                                           "",
                                   style: GoogleFonts.plusJakartaSans(
                                     fontWeight: FontWeight.w400,
@@ -351,7 +394,7 @@ class ProfilePage extends StatelessWidget {
                     ),
                     GestureDetector(
                       onTap: () {
-                        _showLogoutModal(context);
+                        showLogoutDialog(context);
                       },
                       child: Row(
                         children: [
@@ -388,122 +431,17 @@ class ProfilePage extends StatelessWidget {
     );
   }
 
-  void _showLogoutModal(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => const LogoutDialog(),
-    );
-  }
-}
-
-class LogoutDialog extends StatelessWidget {
-  const LogoutDialog({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-    final ColorScheme colors = theme.colorScheme;
-
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(16.0),
-              decoration: const BoxDecoration(
-                color: AppColors.primary,
-                shape: BoxShape.circle,
-              ),
-              child: const Icon(
-                Icons.exit_to_app_rounded,
-                size: 32,
-                color: Colors.white,
-              ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Log Out?',
-              style: theme.textTheme.titleLarge?.copyWith(
-                fontWeight: FontWeight.w500,
-                color: theme.primaryColor,
-              ),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'You will be signed out of your account and returned to the login screen.',
-              style: theme.textTheme.bodyMedium?.copyWith(
-                color: colors.onSurface,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 24),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(8.0),
-                    onTap: () => Navigator.pop(context),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        border: Border.all(
-                          color: colors.outline,
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Cancel',
-                          style: theme.textTheme.labelLarge?.copyWith(
-                            color: colors.onSurfaceVariant,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(width: 16),
-                Expanded(
-                  child: InkWell(
-                    borderRadius: BorderRadius.circular(8.0),
-                    onTap: () async {
-                      getStorageData.removeAllData();
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(vertical: 16),
-                      decoration: BoxDecoration(
-                        color: AppColors.primary,
-                        borderRadius: BorderRadius.circular(8.0),
-                        border: Border.all(
-                          color: colors.error.withOpacity(0.2),
-                        ),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'Logout',
-                          style: theme.textTheme.labelLarge?.copyWith(
-                            color: colors.surface,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
+  void showLogoutDialog(BuildContext context) {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: context,
+        builder: (_) => const LogoutDialogCupertino(),
+      );
+    } else {
+      showDialog(
+        context: context,
+        builder: (_) => const LogoutDialog(),
+      );
+    }
   }
 }
